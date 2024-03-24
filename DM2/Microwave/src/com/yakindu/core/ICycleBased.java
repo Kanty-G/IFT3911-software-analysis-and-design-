eff52dc1536ce2a0bb3468f9d44d409e560047e1
Kanty Gakima <91140943+Kanty-G@users.noreply.github.com>
2024-03-24 15:16:00 -0400

    Add files via upload

---
+++ DM2/Microwave/src/com/yakindu/core/ICycleBased.java
@@ -0,0 +1,10 @@
+package com.yakindu.core;
+
+public class ICycleBased {
+
+    public void runCycle() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'runCycle'");
+    }
+
+}
